9680dc6dd308dbe0c71d3a933bad69abae506d6a
Alejandro Virasoro <74379129+alevirasoro@users.noreply.github.com>
2021-03-26 19:12:40 -0300

    Comentario de descripcion y un check

--- 1a1/Script1a1.sql
+++ 1a1/Script1a1.sql
@@ -1,3 +1,8 @@
+/*
+Base de datos que demuestra la relacion 1a1 entre una tabla de helados y una que contiene las recetas de los mismos.
+Cada helado podra tener solo una receta y cada receta correspondera solo a un helado.
+*/
+
 create database act12
 go
 use act12
@@ -5,7 +10,7 @@ go
 
 create table Helados(
 	idGusto int not null identity(1,1),
-	precioKg money not null,
+	precioKg money not null check(precioKg > 0),
 	descuento float null,
 	stock float not null,
 	primary key(idGusto) 
