b0f2bd10741b65b6423cdc3b70035a1083e05f13
Alejandro Virasoro <74379129+alevirasoro@users.noreply.github.com>
2021-03-26 12:35:34 -0300

    Update Script1a1.sql

--- 1a1/Script1a1.sql
+++ 1a1/Script1a1.sql
@@ -1,1 +1,23 @@
+create database act12
+go
+use act12
+go
 
+create table Helados(
+	idGusto int not null identity(1,1),
+	precioKg money not null,
+	descuento float null,
+	stock float not null,
+	primary key(idGusto) 
+)
+go
+create table Recetas(
+	idGusto int not null identity (1,1),
+	leche float null,
+	azucar float null,
+	huevo smallint null,
+	adicionales nvarchar(150),
+	primary key (idGusto),
+	foreign key (idGusto) references Helados (idGusto),
+)
+go
